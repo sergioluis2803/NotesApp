d668225e01b18054dc73f8aa59818eb55e9d5d83
sergioluis2803 <sergiomv2803@gmail.com>
2024-02-28 16:51:33 -0500

    pantalla de opciones

---
+++ app/src/main/java/com/example/notesapp/ui/options/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.notesapp.ui.options
+
+class Constants {
+
+    companion object {
+        const val IG = "https://www.instagram.com/serch.lmv/"
+        const val X = "https://twitter.com/SergioL41593868"
+        const val GITHUB = "https://github.com/sergioluis2803"
+        const val LINKEDIN = "https://www.linkedin.com/in/sergio-mostacero-91762221b/"
+    }
+}
--- app/src/main/java/com/example/notesapp/ui/options/OptionsFragment.kt
+++ app/src/main/java/com/example/notesapp/ui/options/OptionsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.notesapp.ui.options
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,11 +10,32 @@ import androidx.fragment.app.Fragment
 import com.example.notesapp.databinding.FragmentOptionsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class OptionsFragment : Fragment() {
 
     private var _binding: FragmentOptionsBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ivInstagram.setOnClickListener {
+            openRed(Constants.IG)
+        }
+
+        binding.ivGitHub.setOnClickListener {
+            openRed(Constants.GITHUB)
+        }
+
+        binding.ivX.setOnClickListener {
+            openRed(Constants.X)
+        }
+
+        binding.ivLinkedin.setOnClickListener {
+            openRed(Constants.LINKEDIN)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +43,12 @@ class OptionsFragment : Fragment() {
     ): View {
         _binding = FragmentOptionsBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    private fun openRed(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+
+        startActivity(intent)
     }
 
 }
